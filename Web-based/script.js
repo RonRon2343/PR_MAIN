0cecc9555dcd680a4ffcd1cecdfd11a015139d9a
RonaldJustiniano <bbro55854@gmail.com>
2025-12-16 00:05:55 +0800

    Add files via upload

---
+++ Web-based/script.js
@@ -0,0 +1,43 @@
+//PROGRAMMS
+//BUTTON 1
+const Btn1 = document.getElementById('btn1');
+Btn1.addEventListener ("click", function () {
+    window.location.href = "https://www.w3schools.com/html/default.asp";
+});
+//BUTTON 2
+const Btn2 = document.getElementById('btn2');
+Btn2.addEventListener ("click", function() {
+    window.location.href = "https://www.w3schools.com/css/default.asp";
+});
+//BUTTON 3
+const Btn3 = document.getElementById('btn3');
+Btn3.addEventListener ("click", function() {
+    window.location.href = "https://www.w3schools.com/js/default.asp";
+});
+//BUTTON 4
+const Btn4 = document.getElementById('btn4');
+Btn4.addEventListener ("click", function() {
+    window.location.href = "https://www.w3schools.com/cs/default.asp";
+});
+//BUTTON 5
+const Btn5 = document.getElementById('btn5');
+Btn5.addEventListener ("click", function() {
+    window.location.href = "https://www.w3schools.com/java/default.asp";
+});
+//BUTTON 6
+const Btn6 = document.getElementById('btn6');
+Btn6.addEventListener ("click", function() {
+    window.location.href = "https://www.w3schools.com/phython/default.asp";
+});
+
+//LEARN PROG LANGUAGES BUTTONS
+//BEGINNERS BTN
+const beginnersbtn = document.getElementById('beginnersbtn');
+beginnersbtn.addEventListener ("click", function() {
+    window.location.href = "ForBeginners.html";
+});
+const WebDevbtn = document.getElementById('WebDevbtn');
+WebDevbtn.addEventListener ("click", function() {
+    window.location.href = "LearnProgLanguages.html";
+});
+
